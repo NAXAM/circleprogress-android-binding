d939ae1af49c7488dcfc576f77c1b1c6a2ddba50
Do Thanh Binh <binh@naxam.net>
2017-06-28 10:21:29 +0700

    Add project files.

---
+++ CirclerProgressQs/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace CirclerProgressQs
+{
+    [Activity(Label = "CirclerProgressQs", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
